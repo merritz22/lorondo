144d9ce033241e10522832decfedfb04edda39c1
merritz22 <morritzngueguim43@gmail.com>
2023-10-27 17:00:03 +0100

    including footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/header/HeaderComponent.vue';
+import FooterComponent from './components/footer/FooterComponent.vue';
 </script>
 
 <template>
   <div>
     <HeaderComponent />
     <RouterView />
+    <FooterComponent/>
   </div>
 </template>
